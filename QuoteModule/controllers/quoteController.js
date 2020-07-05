1cfce3c809cb0f83d14f74be451ce4ea97a61edf
ninjaleo <63445543+ninjaleo@users.noreply.github.com>
2020-07-05 16:28:58 +0100

    Updated the Get Quotes
    Done by Vel & Somas

--- QuoteModule/controllers/quoteController.js
+++ QuoteModule/controllers/quoteController.js
@@ -1,6 +1,11 @@
 const { quoteservice } = require('../services')
+const url = require('url');
+const Express = require("express");
 
 const { saveQuote } = quoteservice;
+
+const { getQuoteInfo } = quoteservice;
+
 
 const saveQuoteRequest = async (req, res, next) => {
   try {
@@ -14,12 +19,21 @@ const saveQuoteRequest = async (req, res, next) => {
 
 
 const getQuotes = async (req, res, next) => {
-  const { email } = req.body;
+
+  
+ // const { email } = req;
+
+  var queryparams = url.parse(req.url, true).query;
+  email = queryparams.userEmail;
+
+  console.log("Inside the Controller : the mail ID passed is "+email);
+
   if (email == null) {
     res.sendStatus(401);
   }
   else {
     try {
+      console.log("Inside the Controller : the mail ID passed is"+email);
       const result = await getQuoteInfo(email);
       console.log("Inside Controller: received response:" + result);
       res.send(result);
